caac1cabd310be3a6c0ca3f1bf814b8d9d03823c
Rafael Sandrini <rafael@sandrini.com.br>
2016-08-09 19:40:35 -0300

    Fixed swagger

--- work-at-olist/sandrini_api/urls.py
+++ work-at-olist/sandrini_api/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from sandrini_api.views import ChannelViewSet, schema_view, CategoryViewSet
+from sandrini_api.views import ChannelViewSet, CategoryViewSet
 
 router = routers.DefaultRouter()
 router.register(r'channels', ChannelViewSet, base_name='channels')
 router.register(r'categories', CategoryViewSet, base_name='categories')
 
 urlpatterns = [
-    url('doc/', schema_view),
+    url('^docs/', include('rest_framework_swagger.urls')),
+
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- work-at-olist/sandrini_api/views.py
+++ work-at-olist/sandrini_api/views.py
@@ -1,11 +1,6 @@
-from urllib import response
-
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import viewsets
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework import response, schemas
-from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
 
 from sandrini_api.serializers import ChannelSerializer, CategorySerializer, ChannelDetailSerializer
 from sandrini_test.models import Channel, Category
@@ -24,10 +19,6 @@ class ChannelViewSet(viewsets.ViewSet):
 
 
 class CategoryViewSet(viewsets.ViewSet):
-    #queryset = Category.objects.filter(top_category=None)
-    # queryset = Category.objects.all()
-    # serializer_class = CategorySerializer
-
     def list(self, request):
         instance = Category.objects.filter(top_category=None)
         serializer = CategorySerializer(instance, many=True, context={'request': request})
@@ -37,18 +28,3 @@ class CategoryViewSet(viewsets.ViewSet):
         instance = get_object_or_404(Category, pk=pk)
         serializer = CategorySerializer(instance, context={'request': request})
         return Response(serializer.data)
-
-#
-# class ChannelDetailViewSet(viewsets.ViewSet):
-#
-#     def retrieve(self, request, pk):
-#         channel = get_object_or_404(Channel, pk=pk)
-#         serializer_class = ChannelDetailSerializer(channel, context={'request': request})
-#         return Response(serializer_class.data)
-
-
-@api_view()
-@renderer_classes([OpenAPIRenderer, SwaggerUIRenderer])
-def schema_view(request):
-    generator = schemas.SchemaGenerator(title='Sandrini API')
-    return response.Response(generator.get_schema(request=request))
